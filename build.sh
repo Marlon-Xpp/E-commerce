b97137bad334c7a15c9ea4c0bd80213013d7cd6e
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2024-12-02 16:22:54 -0500

    actualizando los cambios correspondientes ahroa fase de prueba deploy

--- build.sh
+++ build.sh
@@ -1,19 +1,38 @@
+# #!/usr/bin/env bash
+# # Exit on error
+# set -o errexit
+# # Modify this line as needed for your package manager (pip, poetry, etc.)
+# pip install -r requirements.txt
+# # Convert static asset files
+# python manage.py collectstatic --no-input
+
+# python manage.py makemigrations
+
+# # Apply any outstanding database migrations
+# python manage.py migrate
+
+
+# # python manage.py loaddata data.json
+# python manage.py loaddata data_utf8.json
+
+
+
+
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+
+# Instalar dependencias
 pip install -r requirements.txt
-# Convert static asset files
+
+# Convertir archivos estáticos
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
+# Crear migraciones solo para la app 'main' si hay cambios
+python manage.py makemigrations main
 
-# Apply any outstanding database migrations
+# Aplicar migraciones pendientes en todas las apps
 python manage.py migrate
 
-
-# python manage.py loaddata data.json
+# Cargar datos desde el archivo fixture
 python manage.py loaddata data_utf8.json
-
-
-
