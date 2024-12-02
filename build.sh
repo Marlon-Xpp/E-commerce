3c3270b7e12530aad3e236581b46a0f9e516346b
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2024-12-02 17:20:31 -0500

    actualizando los cambios ..

--- build.sh
+++ build.sh
@@ -1,38 +1,13 @@
-# #!/usr/bin/env bash
-# # Exit on error
-# set -o errexit
-# # Modify this line as needed for your package manager (pip, poetry, etc.)
-# pip install -r requirements.txt
-# # Convert static asset files
-# python manage.py collectstatic --no-input
-
-# python manage.py makemigrations
-
-# # Apply any outstanding database migrations
-# python manage.py migrate
-
-
-# # python manage.py loaddata data.json
-# python manage.py loaddata data_utf8.json
-
-
-
-
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-
-# Instalar dependencias
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-# Convertir archivos estáticos
+# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Crear migraciones solo para la app 'main' si hay cambios
-python manage.py makemigrations main
-
-# Aplicar migraciones pendientes en todas las apps
+python manage.py makemigrations
+# Apply any outstanding database migrations
 python manage.py migrate
-
-# Cargar datos desde el archivo fixture
+# python manage.py loaddata data.json
 python manage.py loaddata data_utf8.json
