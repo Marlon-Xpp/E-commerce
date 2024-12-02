fde094518fa778fe7c5308fe55756a708c1c63a2
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2024-12-02 06:39:00 -0500

    elimine el archivo inecesario

--- ecommers/settings.py
+++ ecommers/settings.py
@@ -81,7 +81,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "products",
-    "orders",
     "accounts",
     "payments",
     "main",
@@ -128,6 +127,12 @@ DATABASES = {
         default="postgresql://postgres:postgres@localhost/postgres",
         conn_max_age=600,
     )
+    
+    # esat es la configuracion para trabajr localmente con sqlite3 por defecto
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
