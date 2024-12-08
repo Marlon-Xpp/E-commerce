9b6f6a8c37f3fc3a246efa85a15b900b1433b136
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2024-12-08 11:10:20 -0500

    cambios

--- ecommers/settings.py
+++ ecommers/settings.py
@@ -67,8 +67,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '') # Correo
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')  # Contra
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'mpillacap@autonoma.edu.pe') # Correo
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '10216530')  # Contra
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
@@ -127,16 +127,16 @@ WSGI_APPLICATION = "ecommers.wsgi.application"
 
 DATABASES = {
     # Configurar con el link del render q te da esta es la clave para vinuclar remotamente 
-    # "default": dj_database_url.config(
-    #     default="postgresql://bdecommers_user:F1hm2W85I6XLIemGFQKQxca5m2JQigX1@dpg-ct71v1dumphs73dgl4b0-a.oregon-postgres.render.com/bdecommers",
-    #     conn_max_age=600,
-    # )
+    "default": dj_database_url.config(
+        default="postgresql://bdecommers_user:F1hm2W85I6XLIemGFQKQxca5m2JQigX1@dpg-ct71v1dumphs73dgl4b0-a.oregon-postgres.render.com/bdecommers",
+        conn_max_age=600,
+    )
 
     # esat es la configuracion para trabajr localmente con sqlite3 por defecto
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
